e4c4cc818796333bb9760eaa6ce90aec3a294d33
Ricardo Mestre <ricardo@duplos.org>
2015-07-23 14:26:13 +0100

    Refine modal size, position and spacing

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -12,8 +12,12 @@ module.exports = {
   dataThreshold: 400,
   welcome: {
     title: <h1>Twenty Years of India at Night</h1>,
-    body: (<p>For twenty years, a group of satellites has taken pictures of India every night. Researchers at the University of Michigan extracted nightly light readings for 600,000 villages. The resulting 4.4 billion data points hold <a href='#/stories'>stories of electrification</a> in rural India. <a href='#/about'>Learn more about this project</a> or <a href='http://api.nightlights.io/'>access the data through an open API.</a><img src='graphics/content/timeline-demo.gif' alt='Timeline Demo' width='100%' height='100%' />
-    </p>)
+    body: (
+      <div>
+        <p>For twenty years, a group of satellites has taken pictures of India every night. Researchers at the University of Michigan extracted nightly light readings for 600,000 villages. The resulting 4.4 billion data points hold <a href='#/stories'>stories of electrification</a> in rural India. <a href='#/about'>Learn more about this project</a> or <a href='http://api.nightlights.io/'>access the data through an open API.</a></p>
+        <figure><img src='graphics/content/timeline-demo.gif' alt='Timeline Demo' width='100%' height='100%' /></figure>
+      </div>
+    )
   },
   unsupported: {
     title: <h1>WebGL Not Supported</h1>,
