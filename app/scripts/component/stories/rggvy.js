5f5b6677efbb123a15f9114887e78dd3d08ba773
Ricardo Mestre <ricardo@duplos.org>
2015-07-24 20:11:54 +0100

    Wrap story image in figure tag

--- app/scripts/component/stories/rggvy.js
+++ app/scripts/component/stories/rggvy.js
@@ -11,8 +11,12 @@ let story = {
 
 Over 310 million people in India do not have electricity in their homes, and over 90% of those people live in rural areas. The Rajiv Gandhi Grameen Vidyutikaran Yojana (RGGVY) program is India’s flagship national program to increase electrification throughout the country, especially in rural areas. RGGVY launched in 2005, identifying over 100,000 villages with a population of at least 100 people that did not have access to electricity. The program subsidizes the construction of infrastructure to connect these villages to the existing grid or to provide off-grid distributed energy through generators or alternative renewable energy sources.
 
-![RGGVY in Anagul](graphics/stories/rggvy-anagul.png)
-*At the district level, India Lights gives you the ability to highlight and explore villages that have been beneficiaries of RGGVY. The country-wide data includes 47,000 of the 100,000 participating villages.*
+`}</Markdown>
+  <figure className="align-center">
+    <img src="graphics/stories/rggvy-anagul.png" alt="RGGVY in Anagul" />
+    <figcaption>At the district level, India Lights gives you the ability to highlight and explore villages that have been beneficiaries of RGGVY. The country-wide data includes 47,000 of the 100,000 participating villages.</figcaption>
+  </figure>
+<Markdown>{`
 
 People greatly benefit from access to electricity. Kids can study in the evenings, families can have better access to news, farms can use electric tools, and businesses can stay open later. Switching from kerosene to electric lighting results in better air quality and improved health for family members. The Indian government is committed to working towards universal electricity coverage, with RGGVY as the main program to achieve this goal. India’s huge population and the related population growth makes for a particularly demanding challenge; efforts to expand electrification across the country must reach unconnected areas in addition to keeping pace with population growth in areas that already have access.
 
