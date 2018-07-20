021ec607449097df2ce9ac587fbc67506963f348
Derek Lieu <dl@dereklieu.com>
2018-07-20 14:56:47 -0400

    Create helpers for generating paths

--- app/scripts/component/date-control.js
+++ app/scripts/component/date-control.js
@@ -2,6 +2,7 @@ const React = require('react');
 const t = require('prop-types');
 const { Link } = require('react-router-dom');
 const timespan = require('../lib/timespan');
+const getPath = require('../lib/path');
 const interval = require('../config').interval;
 
 class DateControl extends React.Component {
@@ -15,7 +16,6 @@ class DateControl extends React.Component {
     let valid = timespan.getValid({year: year, month: month});
     let next = timespan.next(valid);
     let prev = timespan.prev(valid);
-    let currentMonth = valid.month + ' / ' + valid.year;
 
     let nextClassName = timespan.isLast(valid) ?
       'bttn-next inactive' : 'bttn-next';
@@ -24,20 +24,14 @@ class DateControl extends React.Component {
 
     let content = region.loading ? '' : (
       <ul className='browse-date'>
-      <li><Link to={level} className={prevClassName} params={{
-        year: prev.year,
-        month: prev.month,
-        state,
-        district,
-        interval
-      }} title='Show previous month'><span>Previous Month</span></Link></li>
-      <li><Link to={level} className={nextClassName} params={{
-        year: next.year,
-        month: next.month,
-        state,
-        district,
-        interval
-      }} title='Show next month'><span>Next Month</span></Link></li>
+        <li><Link to={getPath(prev.year, prev.month, state, district, interval)}
+            className={prevClassName}
+            title='Show previous month'
+            ><span>Previous Month</span></Link></li>
+        <li><Link to={getPath(next.year, next.month, state, district, interval)}
+            className={nextClassName}
+            title='Show next month'
+            ><span>Next Month</span></Link></li>
       </ul>
     );
 
@@ -45,7 +39,7 @@ class DateControl extends React.Component {
     <div className='month-label'>
       <h1 className='spane-title'>
         <small>Now Showing</small>
-        <strong>{currentMonth}</strong>
+        <strong>{valid.month} / {valid.year}</strong>
       </h1>
       {content}
     </div>);
