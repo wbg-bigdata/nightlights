de78824738a082aa18a94884a1bbc2b38c922efd
Vitor George <vitor.george@gmail.com>
2018-08-16 11:04:16 +0100

    Enable SCSS in create-react-app, migrate styles and images

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 // Modules
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // Components
-import Header from './components/header';
+import Header from "./components/header";
+
+// Styles
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <main id='site-body' role='main'>
+        <main id="site-body" role="main">
           {this.props.children}
         </main>
       </div>
     );
   }
 }
-
 
 export default App;
